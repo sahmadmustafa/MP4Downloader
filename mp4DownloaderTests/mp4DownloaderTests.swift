bdb503b3a81118480b72a90095d89030c187c518
Ahmad Mustafa <nasirerum5@gmail.com>
2022-12-06 23:15:39 +0500

    Complete UI and Design

---
+++ mp4DownloaderTests/mp4DownloaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  mp4DownloaderTests.swift
+//  mp4DownloaderTests
+//
+//  Created by sid on 7/29/22.
+//
+
+import XCTest
+@testable import mp4Downloader
+
+class mp4DownloaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ mp4DownloaderUITests/mp4DownloaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mp4DownloaderUITestsLaunchTests.swift
+//  mp4DownloaderUITests
+//
+//  Created by sid on 7/29/22.
+//
+
+import XCTest
+
+class mp4DownloaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
