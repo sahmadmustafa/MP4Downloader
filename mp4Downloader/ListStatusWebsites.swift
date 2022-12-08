71a39ba295dcf0afc8a60a89cb7389515e3f5638
Ahmad Mustafa <nasirerum5@gmail.com>
2022-12-09 03:40:56 +0500

    Cleaning Operation

--- mp4Downloader/ListStatusWebsites.swift
+++ mp4Downloader/ListStatusWebsites.swift
@@ -127,29 +127,7 @@ extension ListStatusWebsites: UICollectionViewDelegate {
 }
 
 
-class SectionHeader: UICollectionReusableView {
+class SectionHeader: UICollectionReusableView
+{
     @IBOutlet weak var label: UILabel!
-
-//     var label: UILabel = {
-//         let label: UILabel = UILabel()
-////         label.textColor = .white
-////         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
-////         label.sizeToFit()
-//         return label
-//     }()
-
-//     override init(frame: CGRect) {
-//         super.init(frame: frame)
-//
-//         addSubview(label)
-//
-////         label.translatesAutoresizingMaskIntoConstraints = false
-////         label.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-////         label.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
-////         label.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-//    }
-
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
 }
